6b205e9ee1481cae873b83104b178d2b98797728
felipelaptrin <paotrindadepao@gmail.com>
2026-02-26 15:22:10 -0300

    use explicit vpc_enabled variable

--- modules/lambda/locals.tf
+++ modules/lambda/locals.tf
@@ -1,7 +1,3 @@
 locals {
   account_id = data.aws_caller_identity.current.account_id
-
-  vpc_enabled = var.vpc_id != "" && length(var.subnet_ids) > 0
-
-  create_security_group = local.vpc_enabled
 }
